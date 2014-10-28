f47d53c9b810d572f93531ba214f9469640aff10
Andrei Brabete <ab686@exeter.ac.uk>
2014-10-28 17:47:10 +0000

    nearly finished...we think...

--- src/Pebbles/PebbleGame.java
+++ src/Pebbles/PebbleGame.java
@@ -16,19 +16,13 @@ class PebbleGame {
 
         private final int name;
         private LinkedList<Pebble> pebbles;
-        private int totalValue;
-        private int totalPebbles;
-        private volatile boolean done = false;
         Random next = new Random();
 
         public Player (int name) {
             this.name = name;
             this.pebbles = new LinkedList<Pebble>();
-            this.totalValue = 0;
-            this.totalPebbles = 0;
+
         }
-
-
 
         int getTotalValue() {
             int total = 0;
@@ -44,50 +38,63 @@ class PebbleGame {
             return this.name;
         }
 
-        int getTotalPebbles() {
-            return this.totalPebbles;
-        }
+        synchronized void discardPebble() {
 
-        Pebble discardPebble() {
-
-            this.totalPebbles--;
             Random random1 = new Random();
             int handIndex = random1.nextInt(9);
-            return this.pebbles.remove(handIndex);
-
+            Pebble discarding = this.pebbles.remove(handIndex);
+            Bag parent = this.pebbles.getLast().getParentBag();
+            if (parent == bagX) {
+                bagA.addPebble(discarding);
+                String message = "Player"+this.getPlayerName()+" has discarded a"+discarding.getWeight()+" to bagA";
+                WriteToFile.write(this.getPlayerName(), message);
+                System.out.println(message);
+            }
+            if (parent == bagY) {
+                bagB.addPebble(discarding);
+                String message = "Player"+this.getPlayerName()+" has discarded a"+discarding.getWeight()+" to bagB";
+                WriteToFile.write(this.getPlayerName(), message);
+                System.out.println(message);
+            }
+            if (parent == bagZ) {
+                bagC.addPebble(discarding);
+                String message = "Player"+this.getPlayerName()+" has discarded a"+discarding.getWeight()+" to bagC";
+                WriteToFile.write(this.getPlayerName(), message);
+                System.out.println(message);
+            }
         }
 
-        void takeInitialPebble(Bag bag) {
+        synchronized void takeInitialPebble(Bag bag) {
 
-            this.totalPebbles++;
+
             Random random1 = new Random();
             int bagIndex = random1.nextInt(bag.sizeOfBag()-1);
             this.pebbles.add(bag.removePebble(bagIndex));
 
         }
 
-        void takePebble() {
+        synchronized void takePebble() {
             String message;
             int randomBag = next.nextInt(2);
 
             if (randomBag == 0){
                 if (bagX.sizeOfBag() !=0) {
                     takeInitialPebble(bagX);
-                    message = "player"+name+" has drown a "+this.pebbles.getLast().getWeight()+" from bag"+bagX.getName();
+                    message = "player"+name+" has drawn a "+this.pebbles.getLast().getWeight()+" from bag"+bagX.getName();
                     WriteToFile.write(name,message);
                     System.out.println(message);
                 }
                 else {
                     if (bagY.sizeOfBag() !=0){
                         takeInitialPebble(bagY);
-                        message = "player"+name+" has drown a "+this.pebbles.getLast().getWeight()+" from bag"+bagY.getName();
+                        message = "player"+name+" has drawn a "+this.pebbles.getLast().getWeight()+" from bag"+bagY.getName();
                         WriteToFile.write(name,message);
                         System.out.println(message);
                     }
                     else {
                         if (bagZ.sizeOfBag() != 0){
                             takeInitialPebble(bagZ);
-                            message = "player"+name+" has drown a "+this.pebbles.getLast().getWeight()+" from bag"+bagZ.getName();
+                            message = "player"+name+" has drawn a "+this.pebbles.getLast().getWeight()+" from bag"+bagZ.getName();
                             WriteToFile.write(name,message);
                             System.out.println(message);
                         }
@@ -101,21 +108,21 @@ class PebbleGame {
             if (randomBag == 1){
                 if (bagY.sizeOfBag() !=0) {
                     takeInitialPebble(bagY);
-                    message = "player"+name+" has drown a "+this.pebbles.getLast().getWeight()+" from bag"+bagY.getName();
+                    message = "player"+name+" has drawn a "+this.pebbles.getLast().getWeight()+" from bag"+bagY.getName();
                     WriteToFile.write(name,message);
                     System.out.println(message);
                 }
                 else {
                     if (bagZ.sizeOfBag() !=0){
                         takeInitialPebble(bagZ);
-                        message = "player"+name+" has drown a "+this.pebbles.getLast().getWeight()+" from bag"+bagZ.getName();
+                        message = "player"+name+" has drawn a "+this.pebbles.getLast().getWeight()+" from bag"+bagZ.getName();
                         WriteToFile.write(name,message);
                         System.out.println(message);
                     }
                     else {
                         if (bagX.sizeOfBag() != 0){
                             takeInitialPebble(bagX);
-                            message = "player"+name+" has drown a "+this.pebbles.getLast().getWeight()+" from bag"+bagX.getName();
+                            message = "player"+name+" has drawn a "+this.pebbles.getLast().getWeight()+" from bag"+bagX.getName();
                             WriteToFile.write(name,message);
                             System.out.println(message);
                         }
@@ -129,21 +136,21 @@ class PebbleGame {
             if (randomBag == 2){
                 if (bagZ.sizeOfBag() !=0) {
                     takeInitialPebble(bagZ);
-                    message = "player"+name+" has drown a "+this.pebbles.getLast().getWeight()+" from bag"+bagZ.getName();
+                    message = "player"+name+" has drawn a "+this.pebbles.getLast().getWeight()+" from bag"+bagZ.getName();
                     WriteToFile.write(name,message);
                     System.out.println(message);
                 }
                 else {
                     if (bagX.sizeOfBag() !=0){
                         takeInitialPebble(bagX);
-                        message = "player"+name+" has drown a "+this.pebbles.getLast().getWeight()+" from bag"+bagX.getName();
+                        message = "player"+name+" has drawn a "+this.pebbles.getLast().getWeight()+" from bag"+bagX.getName();
                         WriteToFile.write(name,message);
                         System.out.println(message);
                     }
                     else {
                         if (bagY.sizeOfBag() != 0){
                             takeInitialPebble(bagY);
-                            message = "player"+name+" has drown a "+this.pebbles.getLast().getWeight()+" from bag"+bagY.getName();
+                            message = "player"+name+" has drawn a "+this.pebbles.getLast().getWeight()+" from bag"+bagY.getName();
                             WriteToFile.write(name,message);
                             System.out.println(message);
                         }
@@ -164,13 +171,12 @@ class PebbleGame {
             for (int i =0;i< 9;i++) {
                 full = PebbleGame.fullestBag(bagX,bagY,bagZ);
                 takeInitialPebble(full);
-                message = "player"+name+" has drown a "+this.pebbles.getLast().getWeight()+" from bag"+full.getName();
+                message = "player"+name+" has drawn a "+this.pebbles.getLast().getWeight()+" from bag"+full.getName();
                 WriteToFile.write(name,message);
                 System.out.println(message);
             }
 
-
-            while (!done) {
+            while (!PebbleGame.done) {
                 if (bagX.sizeOfBag() == 0 && bagA.sizeOfBag() != 0){
                     bagX.transferPebbles(bagA);
                 }
@@ -184,23 +190,36 @@ class PebbleGame {
                 takePebble();
 
 
-                if (totalValue == 100) {
-                    done = true;
-                    message = "player"+name+" has won";
-                    WriteToFile.write(name,message);
-                    System.out.println(message);
-                    this.notifyAll();
+                if (this.getTotalValue() == 100) {
+                    PebbleGame.done = true;
+                    endCredits(this.getPlayerName());
                 }
+
+                discardPebble();
             }
 
+        }
 
+        private synchronized void endCredits(int i){
+            String message;
+            if (this.getTotalValue() == 100) {
+                PebbleGame.done = true;
+                message = "player"+i+" has won";
+                WriteToFile.write(i,message);
+                System.out.println(message);
 
+            }
+            for (int j=1;j<=PebbleGame.playersNr.get(); j++) {
+                message = "player"+j+" has lost, as player"+i+" has won.";
+                if (j != i) {
+                    WriteToFile.write(j,message);
+                }
 
+            }
         }
     }
 
     private static Bag fullest;
-
 
     static Bag bagA = new Bag("A","white");
     static Bag bagB = new Bag("B","white");
@@ -210,14 +229,15 @@ class PebbleGame {
     static Bag bagY = new Bag("Y","black");
     static Bag bagZ = new Bag("Z","black");
 
-
+    static boolean done = false;
+    static AtomicInteger playersNr;
 
     public static void main(String args[]) throws NotEnoughPebblesException, InvalidPlayersNumberException {
 
         fillBag(bagX,args[0]);
         fillBag(bagY,args[1]);
         fillBag(bagZ,args[2]);
-        AtomicInteger playersNr = new AtomicInteger(Integer.parseInt(args[3]));
+        playersNr = new AtomicInteger(Integer.parseInt(args[3]));
         checkPlayersNumber(playersNr.get());
         checkEnoughPebbles(bagX, bagY,bagZ, playersNr.get());
 
@@ -249,6 +269,7 @@ class PebbleGame {
                     break;
                 }
                 Pebble bagFiller = new Pebble(weight);
+                bagFiller.setParentBag(bag);
                 bag.addPebble(bagFiller);
             }
 
@@ -293,6 +314,5 @@ class PebbleGame {
         }
         return fullest;
     }
-
 
 }
